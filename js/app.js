5d04e6ad22ddfb6e8b08740a4feb4283826dfbcf
Alex Perez <redflare@gmail.com>
2017-10-17 18:48:00 -0700

    Finalizing changes for deployment

--- js/app.js
+++ js/app.js
@@ -1,13 +1,31 @@
 var main = function() {
 
-// selects all <a> elements that don't begin with a hash or contain the root, and adds target="_blank"
-  $('a:not([href^="#"], [href^="/"])').attr('target', '_blank');
-
   $('.anchor-row').click(function() {
 
     window.location = '#' + this.id;
 
   });
+  //
+  // $('.anchor-row').click(function(event) {
+  //     event.preventDefault();
+  //
+  //     var target_offset = $('#'+this.id).offset();
+  //     var target_top = target_offset.top;
+  //
+  //     $('html, body').animate({scrollTop:target_top}, 500);
+  //
+  //     window.location = '#' + this.id;
+  //
+  // });
+
+
+  // $(location.hash).addClass('active');
+  //
+  // $('.anchor a').click(function(){
+  //   $('.anchor-row').removeClass('active');
+  //
+  //   $(this).parent().parent().addClass('active');
+  // });
 
 }
 
